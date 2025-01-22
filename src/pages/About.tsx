73f347ffef1b5268bf690619de4a1dcf95cc07b1
Ranjan Vaidya <ranjanvaidya47@gmail.com>
2025-01-22 21:38:06 +0530

    initial code

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-12">
+      <h1 className="text-4xl font-bold text-gray-900 mb-6">About Us</h1>
+    </div>
+  );
+};
+
+export default About;
